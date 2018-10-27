5dde633b94a6f1bde0d61f692733066a31dff5c3
Truongg Nguyen <samuraitruong@hotmail.com>
2018-10-27 18:04:47 +1100

    add substraction and auto generate

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,124 @@
 import * as Random from "random-js";
-import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import * as React from "react";
+import logo from "./logo.svg";
 import TextControl from "./TextControl";
-export interface IProps {
+import "./App.css";
+interface IProps {
     name?: string;
 }
 
-
 interface IState {
-    level: number,
-    items: IItem[],
-    validated: boolean;
+    level : number,
+    items : IItem[],
+    validated : boolean;
+    unresolved : number;
+    total : number;
 }
-class App extends React.Component<IProps, IState> {
-    constructor(props: any) {
+export default class App extends React.Component < IProps,
+IState > {
+    constructor(props : any) {
         super(props);
-        this.state = {
-            items: this.generateMath(),
-            level: 7,
-            validated: false
-        }
-        this.onChange = this.onChange.bind(this);
-        this.validate = this.validate.bind(this);
-        this.reset = this.reset.bind(this);
+        this.state = this.generateMath();
+        this.onChange = this
+            .onChange
+            .bind(this);
+        this.validate = this
+            .validate
+            .bind(this);
+        this.reset = this
+            .reset
+            .bind(this);
     }
-    public generateMath() {
-        const items = new Array<IItem>();
-        for (let lv = 1; lv <= 21; lv++) {
-            const left = Random.integer(1, 10)(Random.engines.browserCrypto)
-            const right = Random.integer(1, 10)(Random.engines.browserCrypto)
+    public generateMath() : IState {
+        const items = new Array < IItem > ();
+        const total = Math.ceil((window.innerWidth / 290) * (window.innerHeight - 50) / 90);
+        for (let lv = 1; lv <= total; lv++) {
+            let left = Random.integer(1, 10)(Random.engines.browserCrypto)
+            let right = Random.integer(1, 10)(Random.engines.browserCrypto)
 
+            const rnd = Random.integer(1, 100)(Random.engines.browserCrypto) % 2;
+            const operator = rnd === 0
+                ? "+"
+                : "-";
+            if (operator === "-" && left < right) {
+                const temp = left;
+                left = right;
+                right = temp;
+            }
             items.push({
-                id: lv,
-                input: "",
-                left,
-                operator: "+",
-                result: left + right,
-                right,
+                id: lv, input: "", left, operator,
+                // tslint:disable-next-line:no-eval
+                result: eval(left + operator + right),
+                right
             })
         }
-        return items;
+        return {items, level: 7, total, unresolved: total, validated: false}
     }
     public reset() {
-        const items = this.generateMath();
-        this.setState({ items, validated: false });
+        const newSolve = this.generateMath();
+        this.setState(newSolve);
     }
     public validate() {
         let items = [...this.state.items];
         items = items.map(x => {
-            x.ok = x.input.toString() === x.result.toString();
+            x.ok = x
+                .input
+                .toString() === x
+                .result
+                .toString();
             return x;
         });
-        this.setState({ items, validated: true });
+        this.setState({items, validated: true});
     }
-    public onChange(id: number, value: string) {
+    public onChange(id : number, value : string) {
         let items = [...this.state.items];
+        let resoleved = 0;
         items = items.map(x => {
             if (x.id === id) {
                 x.input = value;
             }
+            if (x.input) {
+                resoleved++;
+            }
             return x;
         });
-        this.setState({ items });
+        this.setState({
+            items,
+            unresolved: items.length - resoleved
+        });
     }
 
     public render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
+                    <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">Welcome to Kids Math</h1>
                 </header>
                 <div className="App-intro">
                     <div className="Container">
-                        {this.state.items.map((item) => <TextControl {...item} key={item.id} onChange={this.onChange} showed={this.state.validated} />)}
+                        {this
+                            .state
+                            .items
+                            .map((item) => <TextControl
+                                {...item}
+                                key={item.id}
+                                onChange={this.onChange}
+                                showed={this.state.validated}/>)}
 
-                        <button className="button" onClick={this.validate}>Check</button>
+                    </div>
+                    <div className="Buttons">
+                        < button className="button" onClick={this.validate}>
+                            Check
+                        </button>
                         <button className="button" onClick={this.reset}>Reset</button>
+
+                        <span>{this.state.unresolved}
+                        </span>
+
                     </div>
                 </div>
             </div>
-        );
+        )
     }
 }
-
-export default App;
--- src/TextControl.tsx
+++ src/TextControl.tsx
@@ -1,25 +1,39 @@
-import * as React from 'react';
+import * as React from "react";
 interface ITextControlProp extends IItem {
-    onChange: (id: number, value: string) => void;
-    showed: boolean;
+    onChange : (id : number, value : string) => void;
+    showed : boolean;
 }
-export default class TextControl extends React.Component<ITextControlProp> {
-    constructor(props: ITextControlProp) {
+export default class TextControl extends React.Component < ITextControlProp > {
+    constructor(props : ITextControlProp) {
         super(props)
     }
-    public onChange(props: any, ev: any) {
+    public onChange(props : any, ev : any) {
         console.log("text box", ev.target.value);
-        this.props.onChange(this.props.id, ev.target.value);
+        this
+            .props
+            .onChange(this.props.id, ev.target.value);
     }
     public render() {
-        return (<div className="item" key={this.props.id}>
-            <div className="formular">{this.props.left} {this.props.operator} {this.props.right} = </div>
-            <div className="result">
-                <input type="text" value={this.props.input} onChange={this.onChange.bind(this, this.props)} />
-                {this.props.showed && this.props.ok ? <span className="tick ok">✓</span> : null}
-                {this.props.showed && !this.props.ok && <span className="tick error">✗</span>}
+        return (
+            <div className="item" key={this.props.id}>
+                <div className="formular">{this.props.left} {this.props.operator}
+                    {this.props.right}
+                    =
+                </div>
+                <div className="result">
+                    <input
+                        type="text"
+                        value={this.props.input}
+                        onChange={this
+                        .onChange
+                        .bind(this, this.props)}
+                        placeholder="?"/> {this.props.showed && this.props.ok
+                        ? <span className="tick ok">✓</span>
+                        : null}
+                    {this.props.showed && !this.props.ok && <span className="tick error">✗</span>}
 
-            </div>
-        </div >)
+                </div>
+            </div >
+        )
     }
 }
